fd8eb2ac04fdecf219effde68eea232439934d6e
Kristi Laur <kristi@heisi.eu>
2022-05-26 15:59:09 +0300

    Reactive: Set up

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
 
   constructor() { }
 
