023dd2c5fbfb8434f53a13412cd928342a8cf117
Kristi Laur <kristi@heisi.eu>
2022-05-26 14:47:42 +0300

    Setting and Patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,31 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // Vormi eeltäitmisel mingite väärtustega ViewChild ja setValue abil
+    // peame kasutama JS objekti, mis vastab täpselt kogu vormi objektile.
+    // See overrideib kõik kasutaja poolt varem sisestatud väärtused.
+    // See ei pruugi olla kõige sobivam meetod, kuna kirjutab asju üle.
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: 'Nelly',
+    //   gender: 'female'
+    // })
+
+    // Siin saab üle kirjutada ka ainult mõndasid väärtuseid. Struktuur peab
+    // küll selle väljani vastama vormi struktuurile.
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+    // siin toimib setValue ka.
+
+    // setValue - kogu vormi andmete määramiseks
+    // patchValue - mingi vormi osa andmete määramiseks
   }
 
   // onSubmit(form: NgForm) {
